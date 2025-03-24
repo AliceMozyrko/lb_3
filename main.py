e3ba7fe2767ca53685796cc02ddd323137e7aeb5
AliceMozyrko <mozyrkoalisa@gmail.com>
2025-03-24 16:38:12 +0200

    finish

--- main.py
+++ main.py
@@ -1,74 +1,128 @@
-class Device:
-    def __init__(self, name):
-        self.name = name
+# class Device:
+#     def __init__(self, name):
+#         self.name = name
     
-    def process_message(self):
-        return f"Обробка повідомлення для {self.name}"
+#     def process_message(self):
+#         return f"Обробка повідомлення для {self.name}"
+
+# class Radiomodule(Device):
+#     def __init__(self):
+#         super().__init__("Радіомодуль")
+
+# class Buttons(Device):
+#     def __init__(self):
+#         super().__init__("Кнопки")
+
+# class Battery(Device):
+#     def __init__(self):
+#         super().__init__("Батарея")
+
+# class Screen(Device):
+#     def __init__(self):
+#         super().__init__("Екран")
+
+# class Processor(Device):
+#     def __init__(self):
+#         super().__init__("Процесор")
+
+# class Sensor(Device):
+#     def __init__(self):
+#         super().__init__("Сенсор")
+
+# # Класи, які містять компоненти
+# class Radio(Radiomodule, Buttons, Battery):
+#     def __init__(self):
+#         pass
+    
+#     def process_message(self):
+#         return f"{self.radiomodule.process_message()} | {self.buttons.process_message()} | {self.battery.process_message()}"
+
+# class Phone(Radiomodule, Buttons, Battery, Screen, Processor):
+#     def __init__(self):
+#         pass
+#     def process_message(self):
+#         return f"{self.radiomodule.process_message()} | {self.buttons.process_message()} | " \
+#                f"{self.battery.process_message()} | {self.screen.process_message()} | {self.processor.process_message()}"
+
+# class PC(Buttons, Screen, Processor):
+#     def __init__(self):
+#         pass
+    
+#     def process_message(self):
+#         return f"{self.buttons.process_message()} | {self.screen.process_message()} | {self.processor.process_message()}"
+    
+# class Tablet(Battery, Processor, Screen, Sensor):
+#     def __init__(self):
+#         pass
+
+#     def process_message(self):
+#         return f"{self.processor.process_message()} | {self.screen.process_message()} | {self.sensor.process_message()} | {self.battery.process_message()}"
+
+# # Створення об'єктів і перевірка роботи
+# radio = Radio()
+# phone = Phone()
+# pc = PC()
+# tablet = Tablet()
+
+# print("Повідомлення для Радіо:")
+# print(radio.process_message())
+# print("\nПовідомлення для Телефона:")
+# print(phone.process_message())
+# print("\nПовідомлення для Компʼютера:")
+# print(pc.process_message())
+# print("\nПовідомлення для Планшета:")
+# print(tablet.process_message())
+
+class Device:
+    def call (self): return 'Device '
 
 class Radiomodule(Device):
-    def __init__(self):
-        super().__init__("Радіомодуль")
+        def call (self): return 'Радіомодуль '
 
 class Buttons(Device):
-    def __init__(self):
-        super().__init__("Кнопки")
+    def call (self): return 'Кнопки '
 
 class Battery(Device):
-    def __init__(self):
-        super().__init__("Батарея")
+    def call (self): return 'Батарея '
 
 class Screen(Device):
-    def __init__(self):
-        super().__init__("Екран")
+    def call (self): return 'Екран '
 
 class Processor(Device):
-    def __init__(self):
-        super().__init__("Процесор")
+    def call (self): return 'Процесор '
 
 class Sensor(Device):
-    def __init__(self):
-        super().__init__("Сенсор")
+    def call (self): return 'Сенсор '
 
-# Класи, які містять компоненти
-class Radio:
+# Класи, що містять компоненти
+class Radio(Radiomodule, Buttons, Battery):
     def __init__(self):
-        self.radiomodule = Radiomodule()
-        self.buttons = Buttons()
-        self.battery = Battery()
-    
-    def process_message(self):
-        return f"{self.radiomodule.process_message()} | {self.buttons.process_message()} | {self.battery.process_message()}"
+       self.type='Радіо'
+       print(Device(). call ()+super(). call ()+'('+super(Radiomodule,self). call ()+ super(Buttons,self). call ()+') like '+self.type)
 
-class Phone:
+class Phone(Radiomodule, Buttons, Battery, Screen, Processor):
     def __init__(self):
-        self.radiomodule = Radiomodule()
-        self.buttons = Buttons()
-        self.battery = Battery()
-        self.screen = Screen()
-        self.processor = Processor()
-    
-    def process_message(self):
-        return f"{self.radiomodule.process_message()} | {self.buttons.process_message()} | " \
-               f"{self.battery.process_message()} | {self.screen.process_message()} | {self.processor.process_message()}"
+        self.type='Телефон'
+        print(Device(). call ()+super(). call ()
+        +'('+super(Radiomodule,self). call ()
+        + super(Buttons,self). call ()
+        + super(Battery,self). call ()
+        + super(Screen,self). call ()
+        +')'+' like '+self.type)
 
-class PC:
+class PC(Buttons, Screen, Processor):
     def __init__(self):
-        self.buttons = Buttons()
-        self.screen = Screen()
-        self.processor = Processor()
-    
-    def process_message(self):
-        return f"{self.buttons.process_message()} | {self.screen.process_message()} | {self.processor.process_message()}"
-    
-class Tablet:
+        self.type='Компʼютер'
+        print(Device(). call ()+super(). call ()+'('+super(Buttons,self). call ()+ super(Screen,self). call ()+')'+' like '+self.type)
+
+class Tablet(Battery, Processor, Screen, Sensor):
     def __init__(self):
-        self.battery = Battery()
-        self.processor = Processor()
-        self.screen = Screen()
-        self.sensor = Sensor()
-    
-    def process_message(self):
-        return f"{self.processor.process_message()} | {self.screen.process_message()} | {self.sensor.process_message()} | {self.battery.process_message()}"
+        self.type='Планшет'
+        print(Device(). call ()+super(). call ()
+        +'('+super(Battery,self). call ()
+        + super(Processor,self). call ()
+        + super(Screen,self). call ()
+        +')'+' like '+self.type)
 
 # Створення об'єктів і перевірка роботи
 radio = Radio()
@@ -76,11 +130,3 @@ phone = Phone()
 pc = PC()
 tablet = Tablet()
 
-print("Повідомлення для Радіо:")
-print(radio.process_message())
-print("\nПовідомлення для Телефона:")
-print(phone.process_message())
-print("\nПовідомлення для Компʼютера:")
-print(pc.process_message())
-print("\nПовідомлення для Планшета:")
-print(tablet.process_message())
